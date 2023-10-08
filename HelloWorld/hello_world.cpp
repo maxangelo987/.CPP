f80f3af56d88ebc59859eaf3e745c5a6209d00a2
Max Angelo Perin <perinmaks@outlook.ph>
2023-10-08 23:39:25 +0800

    Added code from vscoding

--- HelloWorld/hello_world.cpp
+++ HelloWorld/hello_world.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello Max" << std::endl;
-    system("pause");
-    return 0;
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   int number;
+
+   cout << "Hello World" << endl;   raptor_prompt_variable_zzyz ="Ente number:";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> number;
+   cout << "Number is"+number << endl;
+   cin >> number;
+   
+   return 0;
 }
---
+++ HelloWorld/swapping.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int a = 0, b = 0;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf("Before swap a= %d b= %d\n", a, b);
+
+    // procesS
+    //
+    //
+
+     printf("After swap a= %d b= %d\n", a, b);
+
+  
+    return 0;
+
+}
